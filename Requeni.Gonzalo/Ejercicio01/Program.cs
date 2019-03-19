44e3753a92728afd6d8e339f37277f08ef28290f
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2019-03-19 12:04:43 -0300

    Add files via upload

---
+++ Requeni.Gonzalo/Ejercicio01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Ejercicio_01
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 01";
+
+            int numero;
+            int maximo = 0;
+            int minimo = 0;
+            float promedio = 0;
+
+            for(int i=0; i<5; i++)
+            {
+                Console.Write("Escriba un numero: ");
+                numero = int.Parse(Console.ReadLine());
+                if(i==0)
+                {
+                    maximo = numero;
+                    minimo = numero;
+                }
+                else
+                {
+                    if (numero > maximo)
+                        maximo = numero;
+                    if (numero < minimo)
+                        minimo = numero;
+                }
+                promedio += numero;
+            }
+            promedio = promedio / 5;
+            Console.WriteLine("Maximo: {0}", maximo);
+            Console.WriteLine("Minimo: {0}", minimo);
+            Console.WriteLine("Promedio: {0:#,###.00}", promedio);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Requeni.Gonzalo/proyecto01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            
+            Console.ReadKey();
+            Console.Write("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine(nombre);
+            Console.ReadLine();
+
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
